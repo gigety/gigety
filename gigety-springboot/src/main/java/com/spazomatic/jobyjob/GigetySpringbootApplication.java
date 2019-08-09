b8e5e2be0d35957159adc2d9e9af77d3a46cb16a
samuelsegal <samuelmosessegal@gmail.com>
2019-08-09 17:05:12 -0400

    springboot

---
+++ gigety-springboot/src/main/java/com/spazomatic/jobyjob/GigetySpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.spazomatic.jobyjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigetySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigetySpringbootApplication.class, args);
+	}
+
+}
