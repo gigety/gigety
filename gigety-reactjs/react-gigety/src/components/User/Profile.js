2bfcc9e55bcb58f15e1dd1638cd5d9a86de5f526
samuelsegal <samuelmosessegal@gmail.com>
2019-11-25 12:13:42 -0500

    dockerizer, removed old app

--- gigety-reactjs/react-gigety/src/components/User/Profile.js
+++ gigety-reactjs/react-gigety/src/components/User/Profile.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { useGigUser } from 'redux/hooks/useGigUser';
 const Profile = () => {
 	const { giguser } = useGigUser();
-	return giguser ? <div> {giguser.email}</div> : <div>No profile</div>;
+	return giguser ? <div> {giguser.email}</div> : <div></div>;
 };
 
 export default Profile;
