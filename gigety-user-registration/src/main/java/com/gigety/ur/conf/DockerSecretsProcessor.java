1ef4e476cdf85d8b1d6626bc1c1ea649a15a2eb0
samuelsegal <samuelmosessegal@gmail.com>
2020-05-05 14:50:05 -0400

    devops dockerize stackerize

--- gigety-user-registration/src/main/java/com/gigety/ur/conf/DockerSecretsProcessor.java
+++ gigety-user-registration/src/main/java/com/gigety/ur/conf/DockerSecretsProcessor.java
@@ -16,14 +16,11 @@ import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
 
-import com.gigety.web.api.conf.DockerSecretsProcessor;
-import com.gigety.web.api.exception.GigetyException;
-
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * @author samuelsegal
- * This runs on docker container and retrieves all docker secrets which are set inour docker-compose
+ * @author samuelsegal This runs on docker container and retrieves all docker
+ *         secrets which are set inour docker-compose
  *
  */
 @Slf4j
@@ -32,34 +29,32 @@ public class DockerSecretsProcessor implements EnvironmentPostProcessor {
 	@Override
 	public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
 
-
 		try {
 
 			System.out.println("DOCKER SECRETS:::::::::::::::::::::::::::::");
 
 			String dockerSecretsBindPath = environment.getProperty("docker-secret.bind-path");
-			
-			System.out.println("docker-secret.bind-path: {}" +  dockerSecretsBindPath);
-			
+
+			System.out.println("docker-secret.bind-path: {}" + dockerSecretsBindPath);
+
 			Path bindPath = Paths.get(dockerSecretsBindPath);
-			//Properties props = new Properties();
+			// Properties props = new Properties();
 			Map<String, Object> dockerSecrets = new HashMap<>();
-			try(DirectoryStream<Path> stream = Files.newDirectoryStream(bindPath)){
+			try (DirectoryStream<Path> stream = Files.newDirectoryStream(bindPath)) {
 				System.out.println("got stream:");
-				for(Path entry : stream) {
-					
-					
-					byte [] secretBytes = Files.readAllBytes(entry);
-					String secret = new String(secretBytes,StandardCharsets.UTF_8);
-					//TODO:: Remove this debug even though its just debug
+				for (Path entry : stream) {
+
+					byte[] secretBytes = Files.readAllBytes(entry);
+					String secret = new String(secretBytes, StandardCharsets.UTF_8);
+					// TODO:: Remove this debug even though its just debug
 					System.out.println(secret);
-					System.out.println("Docker Secret :: " + entry.getFileName().toString() + " :: " +  secret);
+					System.out.println("Docker Secret :: " + entry.getFileName().toString() + " :: " + secret);
 					dockerSecrets.put(entry.getFileName().toString(), secret);
 				}
 			}
 			MapPropertySource properties = new MapPropertySource("docker-secrets", dockerSecrets);
 			environment.getPropertySources().addLast(properties);
-			
+
 		} catch (Exception e) {
 			String msg = "An error occurred reading docker secrets";
 			log.error("{} :: {}", msg, e);
